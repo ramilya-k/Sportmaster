0d49241b8b9ea74bcc992406a6c7a388b487352f
ramilya <ramilya.kuramshina@gmail.com>
2019-09-26 11:03:14 +0300

    fixed names of variables, comments deleted

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,28 @@ public class Main {
     public static void main(String[] args) {
         int totalPurchases = 16_000;
         int amount = 950;
-        if (totalPurchases <= 15_000) { // Стандарт
+        if (totalPurchases <= 15_000) {
             int amountForBonus = 1_000;
-            int bonusesPerAmount = 50;
-            int bonus = amount / amountForBonus * bonusesPerAmount;
+            int standardBonusesPerAmount = 50;
+            int standardBonus = amount / amountForBonus * standardBonusesPerAmount;
             int remain = amountForBonus - amount % amountForBonus;
-            System.out.println(bonus);
+            System.out.println(standardBonus);
             System.out.println(remain);
         }
-        if (totalPurchases >= 15_001 && totalPurchases <= 150_000) { // Серебро
+        if (totalPurchases >= 15_001 && totalPurchases <= 150_000) {
             int amountForBonus = 1_000;
-            int bonusesPerAmount = 70;
-            int bonus = amount / amountForBonus * bonusesPerAmount;
+            int silverBonusesPerAmount = 70;
+            int silverBonus = amount / amountForBonus * silverBonusesPerAmount;
             int remain = amountForBonus - amount % amountForBonus;
-            System.out.println(bonus);
+            System.out.println(silverBonus);
             System.out.println(remain);
         }
-        if (totalPurchases >= 150_000) { // Золото
+        if (totalPurchases >= 150_000) {
             int amountForBonus = 1_000;
-            int bonusesPerAmount = 100;
-            int bonus = amount / amountForBonus * bonusesPerAmount;
+            int goldBonusesPerAmount = 100;
+            int goldBonus = amount / amountForBonus * goldBonusesPerAmount;
             int remain = amountForBonus - amount % amountForBonus;
-            System.out.println(bonus);
+            System.out.println(goldBonus);
             System.out.println(remain);
         }
     }
