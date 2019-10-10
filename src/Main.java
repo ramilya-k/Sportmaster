781ff3f9e2d45ee2280fa6e4261bf74ea7f237c6
Рамиля <zizuozishou9>
2019-10-10 15:53:24 +0300

    fixes #2

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,27 @@ public class Main {
     public static void main(String[] args) {
         int totalPurchases = 16_000;
         int amount = 950;
-        if (totalPurchases <= 15_000) {
-            int amountForBonus = 1_000;
-            int standardBonusesPerAmount = 50;
+        int maxStandard = 15_000;
+        int minSilver = 15_001;
+        int maxSilver = 150_000;
+        int amountForBonus = 1_000;
+        int standardBonusesPerAmount = 50;
+        int silverBonusesPerAmount = 70;
+        int goldBonusesPerAmount = 100;
+
+        if (totalPurchases <= maxStandard) {
             int standardBonus = amount / amountForBonus * standardBonusesPerAmount;
             int remain = amountForBonus - amount % amountForBonus;
             System.out.println(standardBonus);
             System.out.println(remain);
         }
-        if (totalPurchases >= 15_001 && totalPurchases <= 150_000) {
-            int amountForBonus = 1_000;
-            int silverBonusesPerAmount = 70;
+        if (totalPurchases >= minSilver && totalPurchases <= maxSilver) {
             int silverBonus = amount / amountForBonus * silverBonusesPerAmount;
             int remain = amountForBonus - amount % amountForBonus;
             System.out.println(silverBonus);
             System.out.println(remain);
         }
-        if (totalPurchases >= 150_000) {
-            int amountForBonus = 1_000;
-            int goldBonusesPerAmount = 100;
+        if (totalPurchases >= maxSilver) {
             int goldBonus = amount / amountForBonus * goldBonusesPerAmount;
             int remain = amountForBonus - amount % amountForBonus;
             System.out.println(goldBonus);
